f7f48d6f7d9f933d24a71f0bde55ee2da1277edc
Ross Aker <ross.aker@gmail.com>
2022-08-24 15:35:00 -0400

    add call to get MLB teams

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+
+  const [teams, setTeams] = useState([]);
+  const [teamsLoaded, setTeamsLoaded] = useState(false);
+
+  const fetchTeams = async () => {
+    try {
+      let response = await fetch('https://api.sportsdata.io/v3/mlb/scores/json/teams?key=787b53b6ed4648dcbc5c12c2b96d9c40');
+      let json = await response.json();
+      console.log(json)
+      return { success: true, data: json };
+    } catch (error) {
+      console.log(error);
+      return { success: false };
+    }
+  }
+
+  useEffect(() => {
+    (async () => {
+      setTeamsLoaded(false);
+      let res = await fetchTeams();
+      if (res.success) {
+        setTeams(res.data);
+        setTeamsLoaded(true);
+      }
+    })();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {teamsLoaded ? (
+        <ul>
+          {teams.map(team => 
+            <li key={team.TeamID}>{team.Key}</li>
+          )}
+        </ul>
+      ) : (<p>loading...</p>)}
     </div>
   );
 }
