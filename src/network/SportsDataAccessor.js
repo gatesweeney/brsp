a5256746a708d3ff5483f426bcfcc7f7bc768067
Ross Aker <akerros@amazon.com>
2022-09-05 14:04:20 -0400

    add teams dropdown, accessor cleanup

---
+++ src/network/SportsDataAccessor.js
@@ -0,0 +1,29 @@
+
+export default class SportsDataAccessor {
+
+    #baseUri
+    #key
+
+    constructor() {
+        this.#baseUri = 'https://api.sportsdata.io/v3/'
+        this.#key = '787b53b6ed4648dcbc5c12c2b96d9c40' // TODO: move to env
+    }
+
+
+    async getActiveTeams() {
+        try {
+            let response = await fetch(this.#buildUri('mlb/scores/json/teams'));
+            let json = await response.json();
+            console.log(`Get active teams response: ${json}`)
+            return { success: true, data: json };
+        } catch (error) {
+            console.log(error);
+            return { success: false };
+        }
+    }
+
+    #buildUri(path) {
+        return `${this.#baseUri}${path}?key=${this.#key}`
+    }
+
+}
